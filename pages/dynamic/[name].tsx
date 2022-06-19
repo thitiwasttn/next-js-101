3969f22a842efab187c1d413182dd1fc69a45b2c
Thitiwas <55071973+thitiwasttn@users.noreply.github.com>
2022-06-19 15:41:24 +0700

    Update [name].tsx

--- pages/dynamic/[name].tsx
+++ pages/dynamic/[name].tsx
@@ -1,10 +1,19 @@
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
+import Layout from "../../components/Layout";
+import Head from "next/head";
 
 const Post = () => {
     const router = useRouter()
-    const { name } = router.query
+    const {name} = router.query
 
-    return <p>Post: {name}</p>
+    return <div>
+        <Layout home={false}>
+            <Head>
+                <title>{name}</title>
+            </Head>
+            <p>Post : {name}</p>
+        </Layout>
+    </div>
 }
 
 export default Post
